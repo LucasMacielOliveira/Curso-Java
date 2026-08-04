8cbea4ae455b656672f3dfbd3bd20ec8673893dd
Lucas <lucasmacieldeoliveira.987@gmail.com>
2026-08-04 16:54:55 -0300

    AULA 01 - ESTRUTURA JAVA

---
+++ src/Aula01/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package Aula01;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println("Meu nome é Lucas!");
+        System.out.println("E esse é meu primeiro codigo em JAVA");
+
+    }
+}
+
+//O package informa em qual pacote aquela classe está guardada.
+
+//A classe PrimeiroPrograma pertence ao pacote aula01_estrutura_java.
+
+// public -> Significa que essa classe pode ser acessada por outras partes do programa.
+
+// class -> Informa ao Java que estamos criando uma classe.
+
+//PrimeiroPrograma -> É o nome da classe, que deve ser o mesmo do arquivo.
+
+// static -> Permite que o metodo seja executado sem precisarmos criar um objeto da classe.
+
+// void -> Significa que o metodo não retorna nenhum valor.
+
+
