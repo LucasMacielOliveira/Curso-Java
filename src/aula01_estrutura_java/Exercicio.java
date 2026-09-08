335dc266b996888356024bccb9d30071219d1860
Lucas <lucasmacieldeoliveira.987@gmail.com>
2026-09-08 09:46:14 -0300

    feat: inicia curso de Java

---
+++ src/aula01_estrutura_java/Exercicio.java
@@ -0,0 +1,14 @@
+package aula01_estrutura_java;
+
+public class Exercicio {
+    public static void main(String[] args){
+        System.out.println("===== Apresentação =====");
+
+        System.out.println("Nome: Lucas Maciel de Oliveira"); // Substitua pelo seu nome
+        System.out.println("Curso: Ciência da Computação"); // Substitua pelo seu curso
+        System.out.println("Tecnologia: Java"); // Substitua pela tecnologia que está estudando
+        System.out.println("Projeto atual: OLS Locações"); // Substitua pelo nome do seu projeto atual
+        System.out.println("Objetivo: Tornar-se desenvolvedor backend."); // Substitua pelo seu objetivo
+
+    }
+}
---
+++ src/aula01_estrutura_java/RevisaoEstrutura.java
@@ -0,0 +1,23 @@
+package aula01_estrutura_java;
+
+public class RevisaoEstrutura {
+    public static void main(String[] args){
+        System.out.println("Ola mundo");
+        System.out.println("Começando os estudos em Java!");
+
+        System.out.print("Objetivo: ");
+        System.out.print("tornar-se desenvolvedor backend.");
+
+    }
+}
+
+// package aula01_estrutura_java;
+// o package quer dizer que o arquivo RevisaoEstrutura.java
+// está dentro da pasta aula01_estrutura_java.
+
+//public class RevisaoEstrutura {
+//}
+//public: a classe pode ser acessada por outras partes da aplicação;
+//class: indica a criação de uma classe;
+//RevisaoEstrutura: nome da classe.
+
