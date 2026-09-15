aed9d99ecc1dd9fc89c4f63b58d1ce8e86f565fb
Lucas <lucasmacieldeoliveira.987@gmail.com>
2026-09-15 08:13:49 -0300

    Aula 06 - Do While

---
+++ src/aula06_DoWhile/DoWhile.java
@@ -0,0 +1,13 @@
+package aula06_DoWhile;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        do {
+            System.out.println("Repetição: " + contador);
+            contador++;
+        } while (contador <= 3);
+        }
+    }
+
